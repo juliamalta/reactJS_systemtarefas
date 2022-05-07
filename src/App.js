22b7191dece8fecd8d0224713daaeb3961d3b93a
juliamalta <juliamalta12@gmail.com>
2022-05-07 12:45:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState ,useEffect , useMemo ,useCallback} from 'react';
+
+import './style.css';
 
 function App() {
+
+
+const [ tarefas, setTarefas ] = useState([
+'Pagar a conta de luz',
+'Estudar React Hooks'
+
+]);
+
+
+const [ input ,setInput ] = useState ('');
+
+
+useEffect(() => {
+
+  const tarefasStorage= localStorage.getItem('tarefas');
+
+  if(tarefasStorage){
+    setTarefas(JSON.parse(tarefasStorage));
+  }
+
+} ,[]);
+
+
+useEffect(()=> {
+  localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}, [tarefas]);
+
+const handleAdd =useCallback(() => {
+
+  setTarefas([...tarefas ,input] )
+    setInput('');
+
+} ,[input ,tarefas]);
+
+const totalTarefas = useMemo (()=>tarefas.length,[tarefas]);
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <ul>
+      {tarefas.map(tarefas => (
+        <li key={tarefas}>{tarefas}</li>
+      ))}
+      
+    </ul>
+    <br/>
+    <strong>voce tem {totalTarefas} tarefas</strong>
+<input type="text" value={input} onChange={(e) =>setInput( e.target.value)} className="input"/>
+<button type="button" onClick={handleAdd}>Adicionar</button>
+
     </div>
   );
 }
